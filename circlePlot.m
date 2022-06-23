52c349ef5dba18917404a0a537f0d1e5ac23a9d9
Satvik Tiwari <74293601+satviktiwari@users.noreply.github.com>
2022-06-23 10:30:04 +0530

    Update circlePlot.m

--- circlePlot.m
+++ circlePlot.m
@@ -2,8 +2,14 @@ fileID1 = fopen('output.txt','r');
 formatSpec1 = '%f';
 A = fscanf(fileID1,formatSpec1);
 
-B=load('data.txt');
-plot(B(:,1),B(:,2))
+% B=load('data.txt');
+% scatter(B(:,1),B(:,2), 'filled')
+
+title('Circle Fitting using N number of points')
+xlabel('x coordinates') 
+ylabel('y coordinates') 
+%legend({'Final Circle','Center Point', 'Data Point'})
+%legend('off')
 
 radius = A(1);
 x_cor = A(2);
@@ -13,6 +19,7 @@ R = radius ;  % Radius
 C = [x_cor y_cor] ; % center 
 x = C(1)+R*cos(th) ; 
 y = C(2)+R*sin(th) ;
-plot(x,y)
+p = plot(x,y,'MarkerFaceColor','red','MarkerEdgeColor','red');
+set(p,{'LineWidth'},{2})
 hold on
-plot(x_cor,y_cor,'o-','MarkerFaceColor','blue','MarkerEdgeColor','red')
+plot(x_cor,y_cor,'o-','MarkerFaceColor','blue','MarkerEdgeColor','blue')
