678e957b688675a2abe19403ac161443304812e1
Satvik Tiwari <74293601+satviktiwari@users.noreply.github.com>
2022-06-23 10:32:07 +0530

    Text Files for writing and reading data

---
+++ plotGraph.py
@@ -0,0 +1,20 @@
+# import matplotlib.pyplot as plt
+
+# plt.axis([900, 1100, 470, 670]) 
+
+# for line in open('data.txt', 'r'):
+#     lines = [i for i in line.split()]
+#     x = (lines[0])
+#     y = (lines[1])
+#     plt.scatter(x, y, color='red', marker='*')
+
+# plt.axis([900, 1100, 500, 1000]) 
+      
+# plt.title("XY Coordinates")
+# plt.scatter(x, y, color='red', marker='*')
+# plt.xlabel('X Coordinates')
+# plt.ylabel('Y Coordinates')
+# plt.yticks(y)
+#plt.plot(x, y, marker = 'o', c = 'g')
+  
+# plt.show()
